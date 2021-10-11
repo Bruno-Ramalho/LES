e1b5e744227fcdb67ce147b6088a120896a9ad88
Osmar-Araujo <araujooa@gmail.com>
2021-10-10 21:19:45 -0300

    Iniciando tela index.html

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
-
+import data from './data';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="grid-container">
+            <header className="row">
+                <div>
+                    <a className="brand" href="index.html">XinG-LinG</a>
+                </div>
+                <div>
+                    <a href="cart.html">Carrinho</a>
+                    <a href="signin.html">Login</a>
+                </div>
+            </header>
+            <main>
+                <div className="row center">
+                    {
+                    data.products.map((product) => (
+                        <div key={
+                                product._id
+                            }
+                            className="card">
+                            <a href={
+                                `/product/${
+                                    product._id
+                                }`
+                            }>
+                                <img className="medium"
+                                    src={
+                                        product.image
+                                    }
+                                    alt={
+                                        product.name
+                                    }/>
+                            </a>
+                            <div className="card-body">
+                                <a href={
+                                    `/product/${
+                                        product._id
+                                    }`
+                                }>
+                                    <h2>{
+                                        product.name
+                                    }</h2>
+                                </a>
+                                <div className="rating">
+                                    <span>
+                                        <i className="fa fa-star"></i>
+                                    </span>
+                                    <span>
+                                        <i className="fa fa-star"></i>
+                                    </span>
+                                    <span>
+                                        <i className="fa fa-star"></i>
+                                    </span>
+                                    <span>
+                                        <i className="fa fa-star"></i>
+                                    </span>
+                                    <span>
+                                        <i className="fa fa-star"></i>
+                                    </span>
+                                </div>
+                                <div className="price">R$ {
+                                    product.price
+                                },00</div>
+                            </div>
+                        </div>
+
+                    ))
+                } </div>
+            </main>
+            <footer className="row center">
+                Cria software Todos os Direitos Reservados
+                <span>&#174;</span>
+            </footer>
+        </div>
+    );
 }
 
 export default App;
