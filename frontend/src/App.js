39f2572bc0019a76fa1224f87cc15dd921411f6b
Osmar-Araujo <araujooa@gmail.com>
2021-10-14 11:50:15 -0300

    Criação do back end e tela Home

---
+++ frontend/src/App.js
@@ -0,0 +1,31 @@
+import {BrowserRouter, Route} from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+
+function App() {
+    return (
+        <BrowserRouter>
+            <div className="grid-container">
+                <header className="row">
+                    <div>
+                        <a className="brand" href="index.html">XinG-LinG</a>
+                    </div>
+                    <div>
+                        <a href="cart.html">Carrinho</a>
+                        <a href="signin.html">Login</a>
+                    </div>
+                </header>
+                <main>
+                    <Route path="/"
+                        component={HomeScreen}
+                        exact></Route>
+                </main>
+                <footer className="row center">
+                    Cria software Todos os Direitos Reservados
+                    <span>&#174;</span>
+                </footer>
+            </div>
+        </BrowserRouter>
+    );
+}
+
+export default App;
