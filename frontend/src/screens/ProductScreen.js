015dcb88128c82b45431c92308ea6019e7dba426
Bruno-Ramalho <bruno_jaelson@hotmail.com>
2021-10-18 13:21:39 -0300

    Tela de produto completa

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Product from "../components/products/Product";
 import Rating from "../components/ratings/Rating";
 import data from "../data";
 
