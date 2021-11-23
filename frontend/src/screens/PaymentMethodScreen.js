4de2c3de551f79d85dd9a033eb26b54c5af7b9d5
Osmar-Araujo <araujooa@gamil.com>
2021-11-22 23:19:38 -0300

    Implementada a tela de pagamento

---
+++ frontend/src/screens/PaymentMethodScreen.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { savePaymentMethod } from "../actions/cartActions";
+import CheckoutSteps from "../components/checkoutSteps/CheckoutSteps";
+
+export default function PaymentMethodScreen(props) {
+    const cart = useSelector((state) => state.cart);
+    const {shippingAddress} = cart;
+    if(!shippingAddress.address){
+        props.history.push('shipping');
+    }
+
+    const [paymentMethod, setPaymentMethod] =useState('PayPaypal');
+    const dispatch = useDispatch();
+    const submitHandler = (e) => {
+        e.preventDefault();
+        dispatch(savePaymentMethod(paymentMethod));
+        props.history.push('/placeorder');
+    };
+  return (
+    <div>
+      <CheckoutSteps step1 step2 step3></CheckoutSteps>
+      <form className="form" onSubmit={submitHandler}>
+        <div>
+          <h1>Método de Pagamento</h1>
+        </div>
+        <div>
+          <div>
+            <input
+              type="radio"
+              id="paypal"
+              value="PayPal"
+              name="paymentMethod"
+              required
+              checked
+              onChange={(e) => setPaymentMethod(e.target.value)}
+            />
+            <label htmlFor="paypal">Paypal</label>
+          </div>
+        </div>
+        <div>
+          <div>
+            <input
+              type="radio"
+              id="stripe"
+              value="Stripe"
+              name="paymentMethod"
+              required
+              onChange={(e) => setPaymentMethod(e.target.value)}
+            />
+            <label htmlFor="stripe">Stripe</label>
+          </div>
+        </div>
+        <div>
+            <button className="primary" type="submit">Continuar</button>
+        </div>
+      </form>
+    </div>
+  );
+}
